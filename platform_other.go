2f8a0a24b43e0c0099699a89c9d3b29106e03425
Yoshiki Shibukawa <shibukawa.yoshiki@dena.jp>
2016-01-26 19:31:53 +0900

    fix panic bug when vertex count is bigger than 65536

--- platform_other.go
+++ platform_other.go
@@ -6,6 +6,8 @@ package nanovgo
 import (
 	"log"
 	"unsafe"
+	"encoding/binary"
+	"math"
 )
 
 type Float float32
@@ -22,7 +24,14 @@ func prepareTextureBuffer(data []byte, w, h, bpp int) []byte {
 func castFloat32ToByte(vertexes []float32) []byte {
 	// Convert []float32 list to []byte without copy
 	var b []byte
-	b = (*(*[1 << 20]byte)(unsafe.Pointer(&vertexes[0])))[:len(vertexes)*4]
+	if len(vertexes) > 65536 {
+		b = make([]byte, len(vertexes)*4)
+		for i, v := range vertexes {
+			binary.LittleEndian.PutUint32(b[4*i:], math.Float32bits(v))
+		}
+	} else {
+		b = (*(*[1 << 20]byte)(unsafe.Pointer(&vertexes[0])))[:len(vertexes)*4]
+	}
 	return b
 }
 
